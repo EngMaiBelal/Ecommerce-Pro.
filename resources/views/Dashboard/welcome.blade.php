caafc695e92b1c08338dbbc2d8e24514908cd7d6
EngMaiBelal <EngMaiBelal>
2024-11-22 03:18:50 +0200

    Dashboard Theme

---
+++ resources/views/Dashboard/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.dashboard.app')
+@section('title', 'Welcome')
+@section('content')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,7 @@ use App\Http\Controllers\CategoryController;
 
 Route::group(['prefix' => LaravelLocalization::setLocale(),'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]], function(){
     Route::get('/', function () {
-        return view('welcome');
+        return view('dashboard.welcome');
     });
-    Route::get('/create-category',[CategoryController::class,'create']);
-    Route::post('/store-category',[CategoryController::class,'store'])->name('category.store'); 
+   
 });
